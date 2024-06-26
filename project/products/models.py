9961a9446ea5488bf4a03ad3faba1eeac55111a6
Abdulkhalek <124366787+Abdulkhalek-1@users.noreply.github.com>
2024-06-26 13:35:46 +0000

    Add: Product model

--- project/products/models.py
+++ project/products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from project.users.models import Seller
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="product_images/")
+    alt_text = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{super().__str__()}-img-{self.pk}"
